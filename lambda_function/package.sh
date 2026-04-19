e58fc4ebbe281a5b32cbd95ffa37f9042e2b33bf
Dr Sawera Hanif <dr.sawerahanif@gmail.com>
2026-04-20 01:57:54 +0500

    Create deployment package script for Lambda function
    This script sets up the package for deployment by installing dependencies and zipping the contents.

--- lambda_function/package.sh
+++ lambda_function/package.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+set -e
 
+rm -rf package lambda_deployment.zip
+mkdir -p package
+
+pip install -r requirements.txt -t package
+cp lambda_function.py package/
+
+cd package
+zip -r ../lambda_deployment.zip .
+cd ..
